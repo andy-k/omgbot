fd64594d5ad7184319b567600da8e4197d684e11
Andy Kurnia <andykurn@gmail.com>
2025-01-18 13:50:14 +0800

    new year

--- build.rs
+++ build.rs
@@ -1,4 +1,4 @@
-// Copyright (C) 2020-2024 Andy Kurnia.
+// Copyright (C) 2020-2025 Andy Kurnia.
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
     prost_build::compile_protos(&["src/macondo.proto"], &["src/"])?;
